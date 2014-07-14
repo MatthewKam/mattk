04100a39d061c4a4347826b2d643aba4e96e1bd7
Matt Kam <kamaeleo@makbook.home>
2014-07-14 04:32:26 -0700

    started skills page

--- app/assets/javascripts/portolios.js
+++ app/assets/javascripts/portolios.js
@@ -1,4 +1,19 @@
-
+$(document).ready(function(){
+    $('a[href="#uno"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body')
+          .animate({scrollTop: targetOffset}, 1000);
+         return false;
+        }
+      }
+    });
+  });
 
 $(document).ready(function(){
     $('a[href="#dos"]').click(function() {
@@ -52,7 +67,7 @@ $(document).ready(function(){
   });
 
   $(document).ready(function(){
-    $('a[href="#uno"]').click(function() {
+    $('a[href="#cinco"]').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
       && location.hostname == this.hostname) {
         var $target = $(this.hash);
